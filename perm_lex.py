7050b9fbb2aada3e3a2cd517df0e8cc830afa360
mferoz1010 <77012139+mferoz1010@users.noreply.github.com>
2021-01-19 06:00:00 -0800

    Update perm_lex.py

--- perm_lex.py
+++ perm_lex.py
@@ -1,3 +1,14 @@
-def perm_gen_lex(a): 
-
- 
+#str -> list
+#Returns a Python list of strings where each string represents a permutation of the input string
+def perm_gen_lex(a):
+    final_list = []
+    if a == '':
+        return []
+    elif len(a) == 1:
+        return [a]
+    else:
+        for char in a:
+            simpler_a = a.replace(char, '')
+            for perm in perm_gen_lex(simpler_a):
+                final_list.append(char + perm)
+        return final_list
