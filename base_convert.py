7fdd8da28e5208f802ce9f8853d46b9fa390a5ab
mferoz1010 <77012139+mferoz1010@users.noreply.github.com>
2021-01-19 06:54:24 -0800

    Update base_convert.py

--- base_convert.py
+++ base_convert.py
@@ -1,3 +1,17 @@
-
+#int int -> str
+#a recursive function that returns a string representing num in the base b
 def convert(num, b):
     """Recursive function that returns a string representing num in the base b"""
+    final_str = ''
+    quotient = num // b
+    remainder = num % b
+    if quotient == 0:
+        final_str += '1'
+        return final_str
+    else:
+        if remainder > 9:
+            remainder = chr(remainder + 55)
+        final_str = str(convert(quotient, b)) + str(remainder)
+        return final_str
+    
+    
